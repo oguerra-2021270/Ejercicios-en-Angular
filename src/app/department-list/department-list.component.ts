effd8578fad5cb05c9145069125c714b2d9c9c44
Ostyn Guerra <oguerra-2021270@kinal.edu.gt>
2023-10-03 14:10:47 -0600

    Proyecto de rutas

--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-list',
   template: `
-    <p>
-      department-list works!
-    </p>
+    <h3>
+      Department List
+    </h3>
+    <ul class="items">
+      <li (click)="onSelect(department)" *ngFor="let department of departments">
+        <span class="badge">{{department.id}}</span> {{department.name}}
+      </li>
+    </ul>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class DepartmentListComponent {
+  departments = [
+    {"id": 1, "name": "Angular"},
+    {"id": 2, "name": "Node"},
+    {"id": 3, "name": "MongoDB"},
+    {"id": 4, "name": "Ruby"},
+    {"id": 5, "name": "Bootstrap"}
+  ]
+  constructor(private router: Router) { }
+
+  onSelect(department) {
+    this.router.navigate(['/departments', department.id]);
+  }
 
 }
